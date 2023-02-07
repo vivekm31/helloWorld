5fff3adf59fff6f2db282b5be0ce8cfda604c87e
vivekmehta <vivekm@terpmail.umd.edu>
2023-02-06 19:50:01 -0500

    Adding my first HTML page

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+    
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Vivek Mehta!'
 
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
