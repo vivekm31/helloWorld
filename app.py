72ddef0e3b33b328e0981a790948a65e63d45e90
vivekmehta <vivekm@terpmail.umd.edu>
2023-02-20 20:52:57 -0500

    Added code to demonstrate a first code change

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-    
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Vivek Mehta! Im adding my first code change'
 
+
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template('hello.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run()
