4e4f12a780e25990647fcc7662e8fb317365668c
vivekmehta <vivekm@terpmail.umd.edu>
2023-03-28 16:56:09 -0400

    Module 8 updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,13 +13,23 @@ def hello():  # put application's code here
     return render_template('hello.html')
 
 
-if __name__ == '__main__':
-    app.run()
-
-
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favoritecourse():
+    fun_courses = [str(request.args.get('subject') + request.args.get('course_number'))]
+
+
+    return render_template('favorite-course.html', courses=fun_courses)
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 
 if __name__ == '__main__':
